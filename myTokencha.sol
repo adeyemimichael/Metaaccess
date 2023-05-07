3dfcc1614c1d514707193c34b3c8b1cd4cd6551f
adeyemimike123 <ayobami732000@gmail.com>
2023-05-07 15:55:28 +0100

    first commit

---
+++ myTokencha.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+// public variable here 
+ contract MyToken {
+ string public tokenName = "MetaToken";
+ string public tokenAbbrev = "MToken";
+ uint public totalSupply = 0;
+
+
+ //mapping variable here 
+ mapping (address => uint ) public balances;
+
+
+ function mint(address _address, uint _value) public {
+totalSupply += _value;
+balances[_address] += _value;
+ }
+
+ //burn function here
+  function burn (address _address, uint _value) public {
+      if (balances [_address] >= _value) {
+totalSupply -= _value;
+balances[_address] -= _value;
+ }
+ }
+ }
+ 
